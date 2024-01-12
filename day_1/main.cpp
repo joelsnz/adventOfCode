9be10aca82465f509dc67d86eb0ef6d184e17f13
joelsnz <joel04slm@gmail.com>
2024-01-12 16:28:46 +0100

    Improve day_1_1 code

--- day_1/main.cpp
+++ day_1/main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <fstream>
 
+int getNumbersQuantity(std::string input);
+void fillArray(std::string input, int *array);
+
 int main() {
     std::ifstream fin;
 
@@ -12,38 +15,24 @@ int main() {
     int size;
     int line_number = 0;
     int result = 0;
+    int number;
 
     while (getline(fin, line)) {
-        int number;
-        size = 0;
 
-        // determine the size of the array of numbers
-        for (int i = 0; i < line.length(); i++) {
-            number = (int)(line[i] - '0');
-            if (number <= 9) {
-                size++;
-            }
-        }
+        size = getNumbersQuantity(line);
 
         // assing size to the array
         pNumbers = new int[size];
-        int index = 0;
 
-        // fill the array of numbers
-        for (int i = 0; i < line.length(); i++) {
-            number = (int)(line[i] - '0');
-            if (number <= 9) {
-                pNumbers[index] = number;
-                index++;
-            }
-        }
+        fillArray(line, pNumbers);
 
         // create the number with the first and last element of the line
         if (size != 1) {
-          line_number = (pNumbers[0] * 10) + pNumbers[index - 1];
+          line_number = (pNumbers[0] * 10) + pNumbers[size - 1];
         } else {
           line_number = (pNumbers[0] * 10) + pNumbers[0];
         }
+
         // add line number to the total
         result += line_number;
 
@@ -53,4 +42,28 @@ int main() {
     // display the result
     std::cout << "Result: " << result << '\n';
 	return 0;
+}
+
+int getNumbersQuantity(std::string input) {
+    /* determine the size of the array of numbers */
+    int quantity = 0;
+    for (int i = 0; i < input.length(); i++) {
+        if ((int)(input[i] - '0') <= 9) {
+            quantity++;
+        }
+    }
+    return quantity;
+}
+
+void fillArray(std::string input, int *array) {
+    /* fill the array of numbers */
+    int position = 0;
+
+    for (int i = 0; i < input.length(); i++) {
+        if ((int)(input[i] - '0') <= 9) {
+            array[position] = (int)input[i] - '0';
+            position++;
+        }
+    }
+    return;
 }
