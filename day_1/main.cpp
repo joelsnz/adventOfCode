881a5ffc1eb6572533b2395c86b7ca109a540691
joelsnz <joel04slm@gmail.com>
2024-01-12 15:45:13 +0100

    Finish day_1_1

--- day_1/main.cpp
+++ day_1/main.cpp
@@ -11,9 +11,13 @@ int main() {
     int *pNumbers = NULL;
     int size;
     int line_number = 0;
+    int result = 0;
 
     while (getline(fin, line)) {
         int number;
+        size = 0;
+
+        // determine the size of the array of numbers
         for (int i = 0; i < line.length(); i++) {
             number = (int)(line[i] - '0');
             if (number <= 9) {
@@ -21,11 +25,11 @@ int main() {
             }
         }
 
-        std::cout << "Size: " << size << '\n';
-        
+        // assing size to the array
         pNumbers = new int[size];
         int index = 0;
-        
+
+        // fill the array of numbers
         for (int i = 0; i < line.length(); i++) {
             number = (int)(line[i] - '0');
             if (number <= 9) {
@@ -33,15 +37,20 @@ int main() {
                 index++;
             }
         }
-/*
-        for (int i = 0; i < size; i++) {
-            std::cout << pNumbers[i];
-        }
-*/
-        delete[] pNumbers;
-        size = 0;
-        std::cout << '\n';
-    }
 
+        // create the number with the first and last element of the line
+        if (size != 1) {
+          line_number = (pNumbers[0] * 10) + pNumbers[index - 1];
+        } else {
+          line_number = (pNumbers[0] * 10) + pNumbers[0];
+        }
+        // add line number to the total
+        result += line_number;
+
+        // delete the pointer
+        delete[] pNumbers;
+    }
+    // display the result
+    std::cout << "Result: " << result << '\n';
 	return 0;
 }
