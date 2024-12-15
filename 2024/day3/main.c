4397a30a455fd2ab8b6930faf4038223e861caed
joelsnz <joel04slm@gmail.com>
2024-12-15 21:34:48 +0100

    Day 3 Part 2

--- 2024/day3/main.c
+++ 2024/day3/main.c
@@ -7,27 +7,44 @@ int main(int argc, char *argv[]) {
     return 1;
   }
 
-  // open file
   FILE *fp = fopen(argv[1], "r");
   if (fp == NULL) {
     printf("Failed to open file\n");
     return 1;
   }
   
-  char line[1024], pattern[] = "mul(";
-  int patternSize = sizeof(pattern);
-  long long int p1 = 0;
+  char line[4096], pattern[] = "mul(";
+  char do_[] = "do()", dont[] = "don't()";
+  int p1 = 0, p2 = 0, enable = 1;
   while(fgets(line, sizeof(line), fp)) {
     char *ptr = line; 
-    while(ptr = strstr(ptr, pattern)) {
-      int op1, op2;
-      char closing;
-      if(sscanf(ptr, "mul(%d,%d%c", &op1, &op2, &closing) == 3 && closing == ')') {
-        p1 += op1 * op2;
-        printf("mul(%d,%d)\n", op1, op2);
+    
+    while(*ptr) {
+      if(strncmp(ptr, do_, strlen(do_)) == 0) {
+        enable = 1;
+        ptr += strlen(do_);
+        continue;
       }
-      ptr += patternSize;
+      if(strncmp(ptr, dont, strlen(dont)) == 0) {
+        enable = 0;
+        ptr += strlen(dont);
+        continue;
+      }
+      if(enable && strncmp(ptr, pattern, strlen(pattern)) == 0) {
+        int op1, op2;
+        char closing;
+        if(sscanf(ptr, "mul(%d,%d%c", &op1, &op2, &closing) == 3 && closing == ')') {
+          p1 += op1 * op2;
+          p2 += op1 * op2 * enable;
+        }
+        ptr += strlen(pattern);
+        continue;
+      }
+      ptr++;
     }
   }
-  printf("%lld\n", p1);
+  printf("%d\n", p1);
+  printf("%d\n", p2);
+  
+  return 0;
 }
