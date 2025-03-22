58f2372fb7a6e7c06095d6b9ca621f1813cfbc25
joelsnz <joel04slm@gmail.com>
2025-03-22 11:15:30 +0100

    others

--- 2024/day3/main.c
+++ 2024/day3/main.c
@@ -13,35 +13,11 @@ int main(int argc, char *argv[]) {
     return 1;
   }
   
-  char line[4096], pattern[] = "mul(";
-  char do_[] = "do()", dont[] = "don't()";
+  char line[4096], find[] = "mul(";
+  char doFind[] = "do()", dontFind[] = "don't()";
   int p1 = 0, p2 = 0, enable = 1;
   while(fgets(line, sizeof(line), fp)) {
-    char *ptr = line; 
-    
-    while(*ptr) {
-      if(strncmp(ptr, do_, strlen(do_)) == 0) {
-        enable = 1;
-        ptr += strlen(do_);
-        continue;
-      }
-      if(strncmp(ptr, dont, strlen(dont)) == 0) {
-        enable = 0;
-        ptr += strlen(dont);
-        continue;
-      }
-      if(enable && strncmp(ptr, pattern, strlen(pattern)) == 0) {
-        int op1, op2;
-        char closing;
-        if(sscanf(ptr, "mul(%d,%d%c", &op1, &op2, &closing) == 3 && closing == ')') {
-          p1 += op1 * op2;
-          p2 += op1 * op2 * enable;
-        }
-        ptr += strlen(pattern);
-        continue;
-      }
-      ptr++;
-    }
+    char *mulp = line;
   }
   printf("%d\n", p1);
   printf("%d\n", p2);
